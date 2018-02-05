7b179c0df7d028d8a38e54f1098685b7d8768174
MrX13415 <mr.x.13415@gmail.com>
2018-02-05 22:21:03 +0100

    Added code of original book ui

--- TypeEx/src/java/net/icelane/typeex/ui/BookUI.java
+++ TypeEx/src/java/net/icelane/typeex/ui/BookUI.java
@@ -1,7 +1,14 @@
 package net.icelane.typeex.ui;
 
-import net.minecraft.client.gui.GuiScreen;
+import net.icelane.typeex.net.minecraft.client.gui.TXGuiScreenBook;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 
-public class BookUI extends GuiScreen {
+public class BookUI extends TXGuiScreenBook {
+
+	public BookUI(EntityPlayer player, ItemStack book, boolean isUnsigned) {
+		super(player, book, isUnsigned);
+		// TODO Auto-generated constructor stub
+	}
 
 }
