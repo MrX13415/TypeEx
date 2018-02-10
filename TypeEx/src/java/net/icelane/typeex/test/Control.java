afccda43c04264a5eb3d1efe80434adb6e42aa91
MrX13415 <mr.x.13415@gmail.com>
2018-02-10 13:51:30 +0100

    Added overwrite mode

--- TypeEx/src/java/net/icelane/typeex/test/Control.java
+++ TypeEx/src/java/net/icelane/typeex/test/Control.java
@@ -17,12 +17,13 @@ public class Control implements AKeyListener{
 	@Override
 	public void keyTyped(KeyInfo keyinfo) {
 		//debug:
-		System.out.println(String.format("%s c: %s a: %s s: %s m: %s",
+		System.out.println(String.format("%s c: %s a: %s s: %s m: %s o: %s",
 				keyinfo.toString(),
 				KeyInfo.isControlHeld(),
 				KeyInfo.isAltHeld(),
 				KeyInfo.isShiftHeld(),
-				KeyInfo.isMetaHeld()));
+				KeyInfo.isMetaHeld(),
+				textinfo.overwrite));
 		
 		if (!keyinfo.getKeyState()) return;
 		
@@ -32,8 +33,14 @@ public class Control implements AKeyListener{
 		boolean keyHandled = KeyHandler.handleKey(keyinfo, textinfo);
 		
 		// handle normal char keys ...
-		if (!keyHandled && keyinfo.getKeyChar() != KeyEvent.CHAR_UNDEFINED) {			
-			textinfo.text = textinfo.firstPart() + keyinfo.getKeyChar() + textinfo.lastPart();
+		if (!keyHandled && keyinfo.getKeyChar() != KeyEvent.CHAR_UNDEFINED) {					
+			String lastPart = textinfo.lastPart();
+			
+			// Handle overwrite mode ...
+			if (textinfo.overwrite && lastPart.length() > 0)
+				lastPart = lastPart.substring(1);
+				
+			textinfo.text = textinfo.firstPart() + keyinfo.getKeyChar() + lastPart;
 			textinfo.cursorPosition++;
 		}
 				
