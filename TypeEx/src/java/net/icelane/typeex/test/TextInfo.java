d8195036cc26af3c243b14d9cbd6412a7cfc806f
MrX13415 <mr.x.13415@gmail.com>
2018-02-10 13:44:33 +0100

    Added overwrite mode and javadoc

--- TypeEx/src/java/net/icelane/typeex/test/TextInfo.java
+++ TypeEx/src/java/net/icelane/typeex/test/TextInfo.java
@@ -1,18 +1,45 @@
 package net.icelane.typeex.test;
 
+/**
+ * Holds text and additional information about cursor and editing. 
+ */
 public class TextInfo {
 	
+	/**
+	 * The Text.
+	 */
 	public String text;
+	/**
+	 * The position of the cursor in the text.
+	 */
 	public int cursorPosition;
-
+	/**
+	 * Weather overwriting of characters is enabled.
+	 */
+	public boolean overwrite; 
+	
+	/**
+	 * Returns the first part of the text, from the beginning to the current cursor position.
+	 * @return The first text part.
+	 */
 	public String firstPart() {
 		return firstPart(text, cursorPosition);
 	}
 	
+	/**
+	 * Returns the last part of the text, from the cursor position to the end of the text.
+	 * @return The last part.
+	 */
 	public String lastPart() {
 		return lastPart(text, cursorPosition);
 	}
 	
+	/**
+	 * Returns the first part of the given text, from the beginning to the given cursor position.
+	 * @param text The text to split.
+	 * @param cursorpos The cursor position to split the text.
+	 * @return The first part of the text.
+	 */
 	public static String firstPart(String text, int cursorpos) {
 		if (text.length() > 0 ){
 			return text.substring(0, cursorpos);
@@ -20,6 +47,12 @@ public class TextInfo {
 		return "";
 	}
 	
+	/**
+	 * Returns the last part of the given text, from the given cursor position to the end of the text.
+	 * @param text The text to split.
+	 * @param cursorpos The cursor position to split the text.
+	 * @return The last part of the text.
+	 */
 	public static String lastPart(String text, int cursorpos) {
 		if (cursorpos < text.length()) {
 			return text.substring(cursorpos, text.length());
