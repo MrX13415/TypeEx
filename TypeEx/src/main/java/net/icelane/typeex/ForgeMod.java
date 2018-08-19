40348988bd00c201b67c416a0661bcf3d273bef5
MrX13415 <mr.x.13415@gmail.com>
2018-08-19 14:09:32 +0200

    Added basic event system

--- TypeEx/src/main/java/net/icelane/typeex/ForgeMod.java
+++ TypeEx/src/main/java/net/icelane/typeex/ForgeMod.java
@@ -1,7 +1,9 @@
 package net.icelane.typeex;
 
+import net.icelane.typeex.event.ItemEvents;
 import net.icelane.typeex.proxy.CommonProxy;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -21,8 +23,7 @@ public class ForgeMod
 
     @SidedProxy(clientSide="net.icelane.typeex.proxy.ClientProxy",
     		    serverSide="net.icelane.typeex.proxy.ServerProxy")
-    public CommonProxy proxy;
-    
+    public static CommonProxy proxy;
     
     public static ForgeMod Instance() {
     	return instance;
@@ -34,9 +35,7 @@ public class ForgeMod
     }
        
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
+    public void init(FMLInitializationEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ItemEvents());
     }
 }
