9d530cf993880f7baafb803b8cc413afd86b3e13
MrX13415 <mr.x.13415@gmail.com>
2018-08-19 17:06:48 +0200

    Changed keycode to bs edition (lwjgl conform)

--- TypeEx/src/main/java/net/icelane/typeex/io/KeyInfo.java
+++ TypeEx/src/main/java/net/icelane/typeex/io/KeyInfo.java
@@ -6,28 +6,32 @@ public class KeyInfo {
 	private char keyChar;
 	private boolean state;
 	
-	private static boolean control; // 17 	
-	private static boolean alt;     // 18
-	private static boolean shift;   // 16
-	private static boolean meta;    // 525
+	private static boolean control;
+	private static boolean alt;
+	private static boolean shift;
+	private static boolean meta;
 	
-	public static final int Shift      = 16;
-	public static final int A          = 65;
-	public static final int C          = 67;
-	public static final int V          = 86;
-	public static final int X          = 88;
+	public static final int Shift      = 42;   // 16
+	public static final int Control    = 29;   // 17
+	public static final int Alt        = 56;   // 18
+	public static final int Meta       = 219;  // 525
 	
-	public static final int Esc        = 27;
-	public static final int Backspace  = 8;
-	public static final int Del        = 127;
-	public static final int Home       = 36;
-	public static final int End        = 35;
-	public static final int Ins        = 155;
+	public static final int A          = 30;   // 65
+	public static final int X          = 45;   // 88 
+	public static final int C          = 46;   // 67
+	public static final int V          = 47;   // 86
+		
+	public static final int Esc        = 1;    // 27
+	public static final int Backspace  = 14;   // 8
+	public static final int Del        = 211;  // 127
+	public static final int Home       = 199;  // 36
+	public static final int End        = 207;  // 35
+	public static final int Ins        = 210;  // 155
 	
-	public static final int ArrowLeft  = 37;
-	public static final int ArrowRight = 39;
-	public static final int ArrowUp    = 38;
-	public static final int ArrowDown  = 40;	
+	public static final int ArrowLeft  = 203;  // 37
+	public static final int ArrowRight = 205;  // 39
+	public static final int ArrowUp    = 200;  // 38
+	public static final int ArrowDown  = 208;  // 40
 	
 	public KeyInfo(int keyCode, char keyChar, boolean state) {
 		super();
@@ -36,13 +40,13 @@ public class KeyInfo {
 		this.state = state;
 		
 		switch (keyCode) {
-		case 17: control = state;
+		case Control: control = state;
 			break;
-		case 18: alt = state;
+		case Alt: alt = state;
 			break;
-		case 16: shift = state;
+		case Shift: shift = state;
 			break;
-		case 525: meta = state;
+		case Meta: meta = state;
 			break;
 		}
 	}
