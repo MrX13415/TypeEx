599f19cf82050527d9acbd37f1118dc794946f5f
MrX13415 <mr.x.13415@gmail.com>
2018-08-20 17:19:50 +0200

    Improved "install" script.

--- TypeEx/install.cmd
+++ TypeEx/install.cmd
@@ -1,3 +1,17 @@
 @echo off
 
+echo.
+echo Directory: %cd%
+echo.
+echo Building ...
+echo.
+
+call gradlew.bat build
+
+echo.
+echo Installing ...
+echo.
+
 copy build\libs\TypeEx-1.0.jar %appdata%\.minecraft\mods
+
+timeout 5
