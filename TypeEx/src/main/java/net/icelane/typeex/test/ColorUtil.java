095d463ee8bd61dbacd9795c85c75bd8eb194fb8
MrX13415 <mr.x.13415@gmail.com>
2018-08-22 19:35:18 +0200

    Added color class

---
+++ TypeEx/src/main/java/net/icelane/typeex/test/ColorUtil.java
@@ -0,0 +1,14 @@
+package net.icelane.typeex.test;
+
+import org.lwjgl.util.Color;
+
+public class ColorUtil {
+
+	public Color get(int argb) {
+		return new Color(
+				(argb >> 16) & 0xFF,
+				(argb >> 8) & 0xFF,
+				(argb >> 0) & 0xFF,
+				(argb >> 24) & 0xFF);
+	}
+}
